9eb37431a6816bda22f05c53e8071c9e9dec266f
Pawel Pluta <pawel.pluta@eo.pl>
2017-10-29 19:24:52 +0100

    Add simple JUnit tests

---
+++ src/test/java/com/pawel/domain/CategoryTest.java
@@ -0,0 +1,33 @@
+package com.pawel.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Pawel on 2017-10-29.
+ */
+public class CategoryTest {
+
+	private Category category;
+	private final Long id = 4L;
+	private final String name = "American";
+
+	@Before
+	public void setUp() {
+		this.category = new Category();
+		this.category.setId(id);
+		this.category.setName(name);
+	}
+
+	@Test
+	public void getId() throws Exception {
+		assertEquals(id, category.getId());
+	}
+
+	@Test
+	public void getName() throws Exception {
+		assertEquals(name, category.getName());
+	}
+}
