d87fc9198c4ef56eba09fba8dca4c1afdcac0915
Pawel Pluta <pawel.pluta@eo.pl>
2017-11-14 23:57:14 +0100

    Handle NumberFormatException

--- src/main/java/com/pawel/controllers/RecipeController.java
+++ src/main/java/com/pawel/controllers/RecipeController.java
@@ -71,4 +71,13 @@ public class RecipeController {
 		modelAndView.addObject("exception", exception);
 		return modelAndView;
 	}
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(NumberFormatException.class)
+	public ModelAndView handleNumberFormatException(Exception exception) {
+		log.error("Handling NumberFormatException");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("400error");
+		modelAndView.addObject("exception", exception);
+		return modelAndView;
+	}
 }
