e6b7f59521a0c4194b2cc082ecf52e2100c7f0f4
Pawel Pluta <pawel.pluta@eo.pl>
2017-11-14 23:37:12 +0100

    Handle not found error

--- src/main/java/com/pawel/controllers/RecipeController.java
+++ src/main/java/com/pawel/controllers/RecipeController.java
@@ -1,14 +1,19 @@
 package com.pawel.controllers;
 
 import com.pawel.commands.RecipeCommand;
+import com.pawel.exceptions.NotFoundException;
 import com.pawel.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Pawel on 2017-11-01.
@@ -54,5 +59,14 @@ public class RecipeController {
 		log.debug("Deleting: " + id);
 		recipeService.deleteById(Long.valueOf(id));
 		return "redirect:/";
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ModelAndView handleNotFound() {
+		log.error("Handling not found exception");
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("404error");
+		return modelAndView;
 	}
 }
